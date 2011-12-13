6078ca0f4ceadb86697cc21e9ab60971bdbe64d5
"Jason Scheirer" <jason.scheirer@gmail.com>
2011-12-13 11:21:07 -0800

    Reprompt logic

--- desktopuisupport/streamwrapper.py
+++ desktopuisupport/streamwrapper.py
@@ -9,6 +9,8 @@ class StreamWrapper(object):
         self._original = original
         self._buffer = u""
         self._stderr = stderr
+        self._reprompt = False
+        self._wroteout = False
     def write(self, *args, **kwargs):
         try:
             self._buffer += u' '.join(a.decode("utf-8", "replace")
@@ -17,18 +19,24 @@ class StreamWrapper(object):
                                       for a in args)
         except:
             pass
+        if self._buffer:
+            self._wroteout = True
         if '\n' in self._buffer:
             outstring, self._buffer = self._buffer.rsplit('\n', 1)
             try:
                 outstring = (outstring.replace('\r\n', '\n')
                                       .replace('\n', '\r\n'))
                 pythonaddins._WriteStringToPythonWindow(outstring,
-                                                        is_error=self._stderr)
+                                             reprompt_when_done=self._reprompt,
+                                             is_error=self._stderr)
             except:
                 return self._original.write(*args, **kwargs)
-    def finalize(self):
-        if self._buffer:
+    def finalize(self, reprompt=False):
+        self._reprompt = reprompt
+        if self._buffer or reprompt:
             self.write('\n')
+    def __bool__(self):
+        return self._wroteout
     def __getattr__(self, attrname):
         if attrname not in ('write', 'finalize'):
             return getattr(self._original, attrname)
@@ -40,8 +48,10 @@ class StreamWrapperContextManager(object):
         sys.stdout = StreamWrapper(sys.stdout)
         sys.stderr = StreamWrapper(sys.stderr, True)
     def __exit__(self, exc_type, exc_val, exc_tb):
-        sys.stdout.finalize()
-        sys.stderr.finalize()
+        sobuffer = bool(sys.stdout)
+        sebuffer = bool(sys.stderr)
+        sys.stdout.finalize(sobuffer and not sebuffer)
+        sys.stderr.finalize(not sobuffer)
         sys.stdout = self._stdout
         sys.stderr = self._stderr
 
