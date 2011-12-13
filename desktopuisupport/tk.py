f2b20b4d570d12c578071802d005232896a00333
"Jason Scheirer" <jason.scheirer@gmail.com>
2011-12-12 17:02:53 -0800

    Random loop tweaks

--- desktopuisupport/tk.py
+++ desktopuisupport/tk.py
@@ -1,19 +1,22 @@
 import call_later
 import Tkinter
 
+import pythonaddins
+
 class TKEventLoop(object):
-    _interval = 0.125
+    _interval = 1
     def __init__(self):
         self._running = False
         self.start()
     def start(self):
         self._running = True
-        call_later.call_later(self.tick, 0.125)
+        call_later.call_later(self.tick, self._interval)
     def stop(self):
         self._running = False
     def tick(self):
-        while Tkinter.tkinter.dooneevent(Tkinter.tkinter.DONT_WAIT):
-            pass
+        #while Tkinter.tkinter.dooneevent(Tkinter.tkinter.DONT_WAIT):
+        #    pass
+        pythonaddins._WriteStringToPythonWindow("Tick {}, {}".format(self._running, self._interval))
         if self._running:
             call_later.call_later(self.tick, self._interval)
 
