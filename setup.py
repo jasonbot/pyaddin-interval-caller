7a028e9017b710fffbd3ba7e63cd206db2dbc740
"Jason Scheirer" <jason.scheirer@gmail.com>
2012-02-02 16:34:59 -0800

    Rename module tickextension
    --HG--
    rename : desktopuisupport/__init__.py => tickextension/__init__.py
    rename : desktopuisupport/call_later.py => tickextension/call_later.py
    rename : desktopuisupport/ontickextension.py => tickextension/ontickextension.py
    rename : desktopuisupport/streamwrapper.py => tickextension/streamwrapper.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
-setup(name='desktopuisupport',
+setup(name='tickextension',
       version='1.0',
-      description='Event loop hooks for Arc*.exe to support wx/Tkinter',
+      description=('Event loop hook to allow ArcGIS Add-In Extensions '
+                   'written in Python to be called at regular intervals'),
       author='Jason Scheirer',
       author_email='jscheirer@esri.com',
-      packages=['desktopuisupport']
+      packages=['tickextension']
      )
