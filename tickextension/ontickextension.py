e78effb35bdcd98bca75f583948d0ef84194e139
"Jason Scheirer" <jason.scheirer@gmail.com>
2014-01-22 13:04:21 -0800

    Use absolute package names

--- tickextension/ontickextension.py
+++ tickextension/ontickextension.py
@@ -1,8 +1,12 @@
+# coding: utf-8
 __all__ = ['TickExtension']
 
-import call_later
+import tickextension.call_later
 
 class TickExtension(object):
+    """An inheritable class for Python Add-In Extensions that will call
+       self.onTimer() at regular intervals. Please refer to the
+       documentation in README.md for usage."""
     interval = 1.0
     def onTimer(self):
         "Override this method in your extension class"
@@ -11,12 +15,12 @@ class TickExtension(object):
         self.startTimer()
     def startTimer(self):
         if getattr(self, 'interval', None):
-            call_later.call_later(self._tick, self.interval)
+            tickextension.call_later.call_later(self._tick, self.interval)
         else:
-            call_later.cancel_call(self._tick)
+            tickextension.call_later.cancel_call(self._tick)
     def _tick(self):
         try:
             self.onTimer()
         finally:
             if getattr(self, 'enabled', False) and getattr(self, 'interval', None):
-                call_later.call_later(self._tick, self.interval)
+                tickextension.call_later.call_later(self._tick, self.interval)
