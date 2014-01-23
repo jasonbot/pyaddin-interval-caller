5d2d616a3aa78873fb89eabf87058cc011e3d059
"Jason Scheirer" <jason.scheirer@gmail.com>
2014-01-23 15:07:12 -0800

    Fix to `__all__`

--- tickextension/__init__.py
+++ tickextension/__init__.py
@@ -1,6 +1,6 @@
 """Implements an extension that can have an onTimer() method."""
 
-__all__ ['TickExtension', 'TKInterLoop']
+__all__ = ['TickExtension', 'TKInterLoop']
 
 from tickextension.ontickextension import TickExtension
 from tickextension.tkinterloop import TKInterLoop
